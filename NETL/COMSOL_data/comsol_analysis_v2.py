9d307ba689f0ff08f7560c90e54359416fdfdfc6
evanraj513 <evanraj513@gmail.com>
2021-03-09 09:33:58 -0800

    updated for week 10

--- NETL/COMSOL_data/comsol_analysis_v2.py
+++ NETL/COMSOL_data/comsol_analysis_v2.py
@@ -37,6 +37,24 @@ else:
 def plot_data(plot_y, file, save_question = 1, plot_x = 0, file_name = 'Unnamed', plot_type=0, plot_ideal=False, title_name = ''):
     '''
     Plots the data contained in the file provided. 
+    
+    plot_y: str
+        name of string trying to plot as y
+    file: str
+        name of csv file containing data
+    save_question: binary
+        turn on to save figures automatically
+    file_name: str
+        what the title name of the plot should be
+    plot_type: 0,1,2
+        0 == all separate plots
+        1 == all with same beta_e overlaid
+        2 == all on same plot
+    plot_ideal: Binary
+        Turns on and off the ideal plots. Will also auto turn on the 2 plots. 
+    title_name: str
+        overrides file_name for title
+    
     '''
     
     if file_name == 'Unnamed':
@@ -69,7 +87,7 @@ def plot_data(plot_y, file, save_question = 1, plot_x = 0, file_name = 'Unnamed'
         key = find_key('ec.Jx*ec.Ex + ec.Jy*ec.Ey + ec.Jz*ec.Ez (W), Joule Heating',key_hal)
         comsol_power = abs(df_hal[key[0]])
         y_plot_name = comsol_power.name
-    elif plot_y == 'Resistor_Joule Heating':
+    elif plot_y == 'Resistor-Joule Heating' or plot_y == 'Resistor - Joule Heating':
         key_R = find_key('ec.Ex*ec.Jx+ec.Ey*ec.Jy+ec.Ez*ec.Jz (W), Resistor Power',key_hal)
         key_J = find_key('ec.Jx*ec.Ex + ec.Jy*ec.Ey + ec.Jz*ec.Ez (W), Joule Heating',key_hal)
         comsol_power = abs(df_hal[key_R[0]]) - abs(df_hal[key_J[0]])
@@ -108,7 +126,8 @@ def plot_data(plot_y, file, save_question = 1, plot_x = 0, file_name = 'Unnamed'
         print('Error. Input not valid.')
         raise Exception
     
-    x_plot_name = x_plot.name
+    # x_plot_name = x_plot.name
+    x_plot_name = 'L [m]'
     
     rs1 = int(beta_e.unique().size) ## Number of beta_e tested
     rs3 = int(x_plot.unique().size) ## Number of L's tested
@@ -198,7 +217,7 @@ def plot_data(plot_y, file, save_question = 1, plot_x = 0, file_name = 'Unnamed'
             elif plot_type == 2:
                 
                 if plot_ideal == False:
-                    ax_hal.plot(x_plot[a][b], comsol_power[a][b],'-',
+                    ax_hal.plot(x_plot[a][b], comsol_power[a][b],'-x',
                             label = r'COMSOL: '+
                             r'$\beta_i = $'+str(round(beta_i[a][b][0],2))+
                             r', $\beta_e = $'+str(round(beta_e[a][0][0],3)))
@@ -227,14 +246,14 @@ def plot_data(plot_y, file, save_question = 1, plot_x = 0, file_name = 'Unnamed'
             if plot_ideal == False:
                 ax_hal.set_ylabel(y_plot_name)
                 ax_hal.set_xlabel(x_plot_name)
-                ax_hal.set_title(file_name + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
+                ax_hal.set_title(title_name + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
                 ax_hal.legend(fontsize = 'x-small')#, loc = 'upper left', bbox_to_anchor=(1.05, 1))
                 
                 fig_hal.set_size_inches(10,10)
             else:
                 ax_hal[0].set_ylabel(y_plot_name)
                 ax_hal[1].set_xlabel(x_plot_name)
-                ax_hal[0].set_title(file_name + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
+                ax_hal[0].set_title(title_name + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
                 ax_hal[0].legend(fontsize = 'x-small')#, loc = 'upper left', bbox_to_anchor=(1.05, 1))
                 ax_hal[1].set_ylabel('Rel. Difference')
                 
@@ -248,14 +267,14 @@ def plot_data(plot_y, file, save_question = 1, plot_x = 0, file_name = 'Unnamed'
         if plot_ideal == False:
             ax_hal.set_ylabel(y_plot_name)
             ax_hal.set_xlabel(x_plot_name)
-            ax_hal.set_title(file_name + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
+            ax_hal.set_title(title_name)# + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
             ax_hal.legend(fontsize = 'x-small')#, loc = 'upper left', bbox_to_anchor=(1.05, 1))
             
             fig_hal.set_size_inches(10,10)
         else:
             ax_hal[0].set_ylabel(y_plot_name)
             ax_hal[1].set_xlabel(x_plot_name)
-            ax_hal[0].set_title(file_name + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
+            ax_hal[0].set_title(title_name)# + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
             ax_hal[0].legend(fontsize = 'x-small')#, loc = 'upper left', bbox_to_anchor=(1.05, 1))
             ax_hal[1].set_ylabel('Rel. Difference')
             
@@ -267,19 +286,94 @@ def plot_data(plot_y, file, save_question = 1, plot_x = 0, file_name = 'Unnamed'
     if saving == 1:
         pdf.close()
         
+    return fig_hal, ax_hal
+        
         
 # file_name='seg_far_L_sens_run1_5'
-file_name = 'seg_far_both_beta_sens_run1'
+# file_name = 'seg_far_v7_linear_B_run1'
+file_name = 'seg_far_v6_2_hall_10_L_sweep'
+# file_name = 'seg_far_v6_2_L_020_100_run1'
 
 file='../comsol_data_v2/'+file_name
+
+plot_y_str = 'Resistor-Joule Heating'
+# plot_y_str = file_name
+# plot_y_str = 'ec.Ey*ec.Jy (W/m^3), Faraday Power'
 
 # plot_all = ['Joule Heating','Resistor_Joule Heating']
 # for x in plot_all:
 #     plot_data(x, file, save_question = 0, plot_x = 0, file_name = file_name+': '+x, plot_type=2,plot_ideal=True)
 
-plot_data('Faraday Power', file, save_question=0, plot_x=0, title_name='Segmented Faraday: Faraday Power',
-          plot_type=2, plot_ideal=False)
+# fig_hal, ax_hal = plot_data(plot_y_str, 
+#           file, save_question=0, plot_x=0, title_name='Segmented Faraday:'+file_name,
+#           plot_type=2, plot_ideal=False)
 
+def comb_plots(file_list, plot_y_str, 
+          save_question=0, plot_x=0, title_name='',
+          plot_type=2, plot_ideal=False):
+    '''
+    Parameters
+    ----------
+    file_list : list of strings
+        names of files to go through and plot, and then combine lines
+    
+    The other parameters are described in the plot_data function
+
+    Returns
+    -------
+    fig: plt.figure
+        combined figure
+    ax: plt.axes
+        axes of combined figure
+    '''
+    
+    fig,ax = plt.subplots(1,1,figsize=(10,10))
+    
+    count = 0
+    
+    for k in file_list:
+        fig1,ax1 = plot_data(plot_y_str, 
+          k, save_question=0, plot_x=0, title_name='Segmented Faraday:'+file_name,
+          plot_type=2, plot_ideal=False)
+        close(fig1)
+        line1 = ax1.get_lines()
+        handles,labels = ax1.get_legend_handles_labels()
+        # for j in np.count_nonzero(line1):
+        elec_num = 2**(count+2)
+        x_plot = line1[0].get_data()[0]
+        y_plot = line1[0].get_data()[1]
+        ax.plot(x_plot,y_plot,
+                label = labels[0]+': '+str(elec_num)+' Electrodes')
+        ymax = max(line1[0].get_data()[1])
+        xpos = np.where(line1[0].get_data()[1] == (ymax))[0][0]
+        xmax = line1[0].get_data()[0][xpos]
+        
+        ax.plot(xmax,ymax,'x')
+        
+        if count == 0:
+            ax.plot(x_plot,np.ones(12)*1.622E6,'--',label='Bz constant, 4 electrodes')
+        if count == 1:
+            ax.plot(x_plot,np.ones(12)*3.003E6,'--', label='Bz constant, 8 electrodes')
+        if count == 2:
+            ax.plot(x_plot,np.ones(12)*5.705E6,'--', label='Bz constant, 16 electrodes')
+        count += 1
+        
+        
+    ax.set_ylabel(plot_y_str)
+    ax.set_xlabel('L [m]')
+    ax.set_title(title_name)# + r' $\beta_e = $'+str(round(beta_e[a][0][0],3)))
+    ax.legend(fontsize = 'x-small')#, loc = 'upper left', bbox_to_anchor=(1.05, 1))
+    ax.set_ylabel('Rel. Difference')
+    
+    return fig,ax
+
+file_list = ['../comsol_data_v2/'+'seg_far_v6_2_hall_10_L_fine_sweep',
+             '../comsol_data_v2/'+'seg_far_v6_3_hall_10_L_fine_sweep',
+             '../comsol_data_v2/'+'seg_far_v6_4_hall_10_L_fine_sweep']
+
+fig,ax = comb_plots(file_list,plot_y_str, 
+           save_question=0, plot_x=0, title_name='Segmented Faraday:'+file_name,
+           plot_type=2, plot_ideal=False)
 
 
 
